66cc4b6783becb717eeb52e72243345ef34a5001
malcolm.k.x <malcolm.k.x@gmail.com>
2012-01-25 10:23:07 +0000

    RabbitIncrementAgeTest.java uitgebreid met testen of rabbit leeft en of rabbit dood kan gaan;

--- test/RabbitIncrementAgeTest.java
+++ test/RabbitIncrementAgeTest.java
@@ -9,33 +9,52 @@ import org.junit.Test;
 
 public class RabbitIncrementAgeTest {
 	
-	// rabbit
-	private Rabbit rabbit;
-	// a list to add newly born rabbits to
-	private List<Animal> newRabbits;
+	// proefkonijn 1
+	private Rabbit rabbitOne;
+	// proefkonijn 2
+	private Rabbit rabbitTwo;
+	// proefkonijn 3
+	private Rabbit rabbitThree;
+	// proefkonijn 4
+	private Rabbit rabbitFour;
 	
 	@Before
 	public void setUp() throws Exception {
-		this.rabbit = new Rabbit(false, new Field(20, 20), new Location(10, 10));
-		this.newRabbits = new ArrayList<Animal>();
+		this.rabbitOne = new Rabbit(false, new Field(20, 20), new Location(10, 10));
+		this.rabbitTwo = new Rabbit(false, new Field(20, 20), new Location(10, 10));
+		this.rabbitThree = new Rabbit(false, new Field(20, 20), new Location(10, 10));
+		this.rabbitFour = new Rabbit(false, new Field(20, 20), new Location(10, 10));
 	}
 
 	@Test
 	public void testIncrementingAge() {
 		// test of het ophogen van de leeftijd ook daadwerkelijk gebeurd
-		rabbit.incrementAge();
-		assertEquals("Leeftijd niet juist opgehoogt", 1, rabbit.getAge());
+		rabbitOne.incrementAge();
+		assertEquals("Leeftijd niet juist opgehoogt", 1, rabbitOne.getAge());
 	}
 	
 	@Test
 	public void testIncrementingAgeFourTimes()
 	{
 		// test of het ophogen vier keer lukt;
-		for(int i = 1; i < 5; i++) {
-			rabbit.incrementAge();
+		for(int i = 0; i < 5; i++) {
+			rabbitTwo.incrementAge();
 		}
 		
-		assertEquals("Leeftijd niet juist opgehoogd na 4 keer", 4, rabbit.getAge());
+		assertEquals("Leeftijd niet juist opgehoogd na 4 keer", 4, rabbitTwo.getAge());
+	}
+	
+	@Test
+	public void testRabbitAlive()
+	{
+		assertEquals("Konijn is een zombie", true, rabbitThree.isAlive());
+	}
+	
+	@Test
+	public void testDieingRabbit()
+	{
+		rabbitFour.setDead();
+		assertEquals("Konijn is onsterfelijk", false, rabbitFour.isAlive());
 	}
 
 }
