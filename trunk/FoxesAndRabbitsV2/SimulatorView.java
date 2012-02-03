6a1a21c50253396d69824084a58fbcf41547d742
haroldholsappel@gmail.com <haroldholsappel@gmail.com@8f4c0ede-6d92-6506-6a85-768083a2494e>
2012-02-03 09:00:30 +0000

    Alle warnings opgelost... in salpeterzuur

--- trunk/FoxesAndRabbitsV2/SimulatorView.java
+++ trunk/FoxesAndRabbitsV2/SimulatorView.java
@@ -19,7 +19,12 @@ import java.util.Map;
  */
 public class SimulatorView extends JFrame
 {
-    // Colors used for empty locations.
+    /**
+	 * 
+	 */
+	private static final long	serialVersionUID	= 1L;
+
+	// Colors used for empty locations.
     private static final Color EMPTY_COLOR = Color.white;
 
     // Color used for objects that have no defined color.
@@ -31,7 +36,7 @@ public class SimulatorView extends JFrame
     private FieldView fieldView;
     
     // A map for storing colors for participants in the simulation
-    private Map<Class, Color> colors;
+    private Map<Class<?>, Color> colors;
     // A statistics object computing and storing simulation information
     private FieldStats stats;
     
@@ -80,7 +85,7 @@ public class SimulatorView extends JFrame
     public SimulatorView(int height, int width)
     {
     	stats = new FieldStats();
-        colors = new LinkedHashMap<Class, Color>();
+        colors = new LinkedHashMap<Class<?>, Color>();
 
         setTitle("Fox and Rabbit Simulation");
         stepLabel = new JLabel(STEP_PREFIX, JLabel.CENTER);
@@ -149,7 +154,7 @@ public class SimulatorView extends JFrame
      * @param animalClass The animal's Class object.
      * @param color The color to be used for the given class.
      */
-    public void setColor(Class animalClass, Color color)
+    public void setColor(Class<?> animalClass, Color color)
     {
         colors.put(animalClass, color);
     }
@@ -157,7 +162,7 @@ public class SimulatorView extends JFrame
     /**
      * @return The color to be used for a given class of animal.
      */
-    private Color getColor(Class animalClass)
+    private Color getColor(Class<?> animalClass)
     {
         Color col = colors.get(animalClass);
         if(col == null) {
@@ -222,7 +227,12 @@ public class SimulatorView extends JFrame
      */
     private class FieldView extends JPanel
     {
-        private final int GRID_VIEW_SCALING_FACTOR = 6;
+        /**
+		 * 
+		 */
+		private static final long	serialVersionUID	= 1L;
+
+		private final int GRID_VIEW_SCALING_FACTOR = 6;
 
         private int gridWidth, gridHeight;
         private int xScale, yScale;
