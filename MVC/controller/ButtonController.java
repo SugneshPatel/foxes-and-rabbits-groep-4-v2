0f768daaaa9fb1b3687204872903c22f7910c8a3
vellingamarco <vellingamarco@gmail.com>
2012-02-08 11:55:53 +0000

    comments voor controllers

--- MVC/controller/ButtonController.java
+++ MVC/controller/ButtonController.java
@@ -13,90 +13,127 @@ import javax.swing.border.EmptyBorder;
 import model.Simulator;
 
 /**
- * The abstract controller that serves for all it's subclasses.
- * It initiates a brain that can be used by all the subclasses.
+ * This buttoncontroller makes a JPanel with the buttons used
+ * on the main window of the application. It has 5 buttons:
+ * The first does 1 step of the simulation
+ * The second does 100 steps of the simulation
+ * The third runs the simulation continues
+ * The fourth stops the simulation
+ * The fifth resets the simulation
  * 
  * @author Marco
+ * @author Malcolm
+ * @author Harold
  * @version 2012.02.06
  */
 public class ButtonController extends AbstractController implements ActionListener {
 	
 	private static final long serialVersionUID = -4557571779645953349L;
 	
-	
+	// instance variables 
 	private JButton stepOneButton, stepHundredButton, runButton, stopButton, resetButton;
 	
-	
-	public ButtonController(Simulator brain){
+	/**
+	 * The constructor for the controller.
+	 * @param brain Used for the super constructor
+	 */
+	public ButtonController(Simulator brain) {
 		super(brain);
 	}
 	
-	public JPanel getButtons(){
+	/**
+	 * The implemented method to return a JPanel that can be used
+	 * in the layout of the required window.
+	 * @return JPanel
+	 */
+	@Override
+	public JPanel getPanel() {
+		
+		// the button that does 1 step
 		stepOneButton = new JButton("Step 1");
         stepOneButton.setPreferredSize(new Dimension(90,25));
         stepOneButton.setMinimumSize(new Dimension(90,10));
         stepOneButton.setMaximumSize(new Dimension(90,25));
         stepOneButton.addActionListener(this);
         
-        
+        // the button that does 100 steps
         stepHundredButton = new JButton("Step 100");
         stepHundredButton.setPreferredSize(new Dimension(90,25));
         stepHundredButton.setMinimumSize(new Dimension(90,10));
         stepHundredButton.setMaximumSize(new Dimension(90,25));
         stepHundredButton.addActionListener(this);
         
+        // the button that does a continues run
         runButton = new JButton("Run");
         runButton.setPreferredSize(new Dimension(90,25));
         runButton.setMinimumSize(new Dimension(90,10));
         runButton.setMaximumSize(new Dimension(90,25));
         runButton.addActionListener(this);
         
+        // the button that stops the continues run
         stopButton = new JButton("Stop");
         stopButton.setPreferredSize(new Dimension(90,25));
         stopButton.setMinimumSize(new Dimension(90,10));
         stopButton.setMaximumSize(new Dimension(90,25));
         stopButton.addActionListener(this);
         
+        // the button that resets the simulation
         resetButton = new JButton("Reset");
         resetButton.setPreferredSize(new Dimension(90,25));
         resetButton.setMinimumSize(new Dimension(90,10));
         resetButton.setMaximumSize(new Dimension(90,25));
         resetButton.addActionListener(this);
         
-        
-        //nieuw panel waarin buttons komen
+        // a JPanel that contains the buttons
         JPanel buttonsLeft = new JPanel();
         buttonsLeft.setBorder(new EmptyBorder(6, 6, 6, 6));
         buttonsLeft.setLayout(new BoxLayout(buttonsLeft, BoxLayout.PAGE_AXIS));
         
-        //buttons toevoegen aan buttonsleft panel
+        // a the buttons to the JPanel 
         buttonsLeft.add(stepOneButton);
         buttonsLeft.add(stepHundredButton);
         buttonsLeft.add(runButton);
         buttonsLeft.add(stopButton);
         buttonsLeft.add(resetButton);
         
+        // return the whole panel with all the buttons
         return buttonsLeft;
 	}
 	
+	/**
+	 * The handler for the action events
+	 */
 	public void actionPerformed(ActionEvent e) {
+		
+		// does 1 step
 		if (e.getSource()==stepOneButton) {
 			brain.simulate(1);
 		}
+		
+		// does 100 steps
 		if (e.getSource()==stepHundredButton) {
 			brain.simulate(100);
 		}
+		
+		// continues run
 		if (e.getSource()==runButton) {
 			brain.start();
 		}
+		
+		// stop continues run
 		if (e.getSource()==stopButton) {
 			brain.stop();
 		}
+		
+		// reset simulation
 		if (e.getSource()==resetButton) {
 			brain.reset();
 		}
 	}
-
+	
+	/**
+	 * Unimplemented method
+	 */
 	@Override
 	public JMenuBar getMenu() {
 		// TODO Auto-generated method stub
--- MVC/controller/MenuController.java
+++ MVC/controller/MenuController.java
@@ -13,70 +13,112 @@ import javax.swing.JPanel;
 
 import model.Simulator;
 
+/**
+ * This menucontroller makes a JMenu for the main window of the application
+ * It contains the navigation of the application as well as functions
+ * to close or open a new window with options/information
+ * 
+ * @author Marco
+ * @author Malcolm
+ * @author Harold
+ * @version 2012.02.07
+ */
 public class MenuController extends AbstractController implements ActionListener {
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 4698842680020675242L;
+	
+	// instance variables
 	private JMenuItem quitItem, configItem;
 	
+	/**
+	 * The constructor for the controller.
+	 * @param brain Used for the super constructor
+	 */
 	public MenuController(Simulator brain){
 		super(brain);
 	}
-
+	
+	/**
+	 * Unused implemented method
+	 */
 	@Override
-	public JPanel getButtons() {
+	public JPanel getPanel() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Returns a JMenuBar that can be used in the main window
+	 * @return JMenuBar
+	 */
 	@Override
 	public JMenuBar getMenu() {
+		
+		// menubar
 		JMenuBar menubar = new JMenuBar();
     	
-    	JMenu menu1 = new JMenu("Opties");
+		// menu's
+    	JMenu menu1 = new JMenu("Options");
     	JMenu helpMenu = new JMenu("Help");
     	
-    	//quit item
-    	quitItem = new JMenuItem("Afsluiten");
-    	quitItem.addActionListener(this);
+    	// items
     	configItem = new JMenuItem("Config");
     	configItem.addActionListener(this);
- 
+    	quitItem = new JMenuItem("Quit");
+    	quitItem.addActionListener(this);
     	
+    	// add items to a menu
+    	menu1.add(quitItem);
+    	menu1.add(configItem);
+    	
+    	// add menu's to the menubar
     	menubar.add(menu1);
     	menubar.add(helpMenu);
     	
-    	//menu items toevoegen aan menu1
-    	menu1.add(quitItem);
-    	menu1.add(configItem);
-    
+    	// return the complete menubar
     	return menubar;
 	}
-	
+	/**
+	 * Process the actionevents corresponding to there names
+	 * @param e An actionevent
+	 */
+	@Override
 	public void actionPerformed(ActionEvent e) {
+		// close the application
 		if (e.getSource()==quitItem) {
 			quit();
 		}
+		
+		// open the config window
 		if (e.getSource()==configItem) {
 			config();
 		}
 	}
 	
+	/**
+	 * Method that close the application
+	 */
 	private void quit(){
 		System.exit(0);
 	}
 	
+	/**
+	 * Method that makes a new frame and put's the JPanel 
+	 * that is made by the config controller in it's contentpane
+	 */
 	private void config(){
+		
+		// new frame
 		JFrame config = new JFrame("Config");
 		
+		// new config controller
 		AbstractController configController = new ConfigController(brain);
 		
+		// container
 		JPanel container = (JPanel)config.getContentPane();
-		
 		container.setLayout(new BorderLayout());
-		container.add(configController.getButtons(), BorderLayout.CENTER);
-		
+		// add JPanel created in configcontroller
+		container.add(configController.getPanel(), BorderLayout.CENTER);
 		config.pack();
 		config.setVisible(true);
 	}
