a882c7ca3680185e287e7c3e6c7c23e20c4772c7
vellingamarco <vellingamarco@gmail.com>
2012-02-09 16:19:21 +0000

    Andere runner toegevoegd ivm export

--- MVC/main/SimulationRunner.java
+++ MVC/main/SimulationRunner.java
@@ -23,12 +23,6 @@ public class SimulationRunner extends JFrame {
 	
 	private static final long serialVersionUID = 6038091542956952261L;
 	
-	// main method
-	public static void main(String[] args)
-	{
-		new SimulationRunner();
-	}
-	
 	// instance variables
 	private Simulator brain;
 	private AbstractView simulatorview;
