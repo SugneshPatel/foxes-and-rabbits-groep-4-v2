82f8de319aa0821b344691be90aa9c4b8a1681bc
vellingamarco@gmail.com <vellingamarco@gmail.com@8f4c0ede-6d92-6506-6a85-768083a2494e>
2012-02-09 16:19:21 +0000

    Andere runner toegevoegd ivm export

---
+++ trunk/MVC/runner/Runner.java
@@ -0,0 +1,14 @@
+package runner;
+
+import main.SimulationRunner;
+
+/**
+ * The Runner for the simulation
+ *
+ */
+public class Runner {
+	
+	public static void main(String[] args) {
+		new SimulationRunner();
+	}
+}
